8b3870b3b8b08c5c085f4c5a6f1cecad558bf4eb
JNelson <nelson.jimmy@gmail.com>
2018-03-27 14:30:51 -0600

    user can edit and update student name

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,6 +20,19 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def student_params
