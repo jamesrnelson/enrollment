122d4d28eafe2e47b6ce01e0c516b1f14227c80c
JNelson <nelson.jimmy@gmail.com>
2018-03-27 13:36:46 -0600

    student model tests passing

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 describe Student, type: :model do
   describe 'Validations' do
-    
+    it 'should be valid with a name' do
+      student1 = Student.new(name: 'Jerry')
+
+      expect(student1).to be_valid
+    end
+
+    it 'should be invalid without a name' do
+      student1 = Student.new(name: nil)
+
+      expect(student1).to be_invalid
+    end
   end
 end
